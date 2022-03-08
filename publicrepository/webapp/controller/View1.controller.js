e23f185eb207a072e8668291bdce6aacbdeefd53
Charansamanchi <charan.samanchi@sap.com>
2022-03-08 17:18:49 +0000

    commit1

---
+++ publicrepository/webapp/controller/View1.controller.js
@@ -0,0 +1,56 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, UIComponent) {
+        "use strict";
+        var oArgs; var oView;
+        return Controller.extend("publicrepository.controller.View1", {
+            onInit: function () {
+                debugger;
+                var oRouter = this.getOwnerComponent().getRouter();
+			   oRouter.getRoute("RouteView1").attachPatternMatched(function (oEvent) {
+                debugger;
+              oArgs = oEvent.getParameter("arguments");
+            },this);
+                this.oPage = this.getView().byId("page");
+                this.getComponentContainer().setSettings(this.getComponentSettings());
+            },
+
+            _onRouteMatched : function (oEvent) {
+                debugger;
+              oArgs = oEvent.getParameter("arguments");
+               var oView = this.getView();
+            },
+
+            getComponentSettings: function(oArgs) {
+                debugger;
+                return  {
+                  destinationPath: "/public",
+                  publicLinkId: oArgs.pubLinkId
+                };
+              },
+      
+              getComponentContainer: function () {
+                return this.oPage.getContent()[0];
+            },
+            onComponentCreated: function(oEvent) {
+                debugger;
+                //this._oDocumentTable = oEvent.getParameter("component");
+                // OPTIONAL: set a folder as home folder
+                //this._oDocumentTable.setHomeFolder(<OBJECT_ID>);
+                // OPTIONAL: request a navigation to a repository & folder during runtime
+                //this._oDocumentTable.requestNavigationAndOpen("145f82b9-2bd6-4975-b163-754a93ab9901", "pmPyGBhQcogZX15tYmMlBPhc1dBRBpB2L1EhpQx5aF8");
+        
+              this._oDocumentTable = oEvent.getParameter("component");
+              this._loadRepositoryAndObjectId();
+              },
+        
+              _loadRepositoryAndObjectId: function (oArgs) {
+                this._oDocumentTable.requestNavigationAndOpen('145f82b9-2bd6-4975-b163-754a93ab9901', oArgs.objId);
+            }
+        });
+    });
